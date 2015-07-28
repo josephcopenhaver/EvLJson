a84b42771605c9a96a1400c70ec2282c6bb74e79
Joe Copenhaver <joseph.copenhaver@gmail.com>
2015-07-28 07:17:08 -0500

    1. being less strict on integers less or equal to zero ( support for [-0] ) 2. first pass of test cases

--- src/EvLJson/Parser_test.go
+++ src/EvLJson/Parser_test.go
@@ -6,6 +6,8 @@ import (
 	"testing"
 )
 
+const LOG_STMT_FMT = "%s\n"
+
 var BENCHMARK_BYTES []byte
 
 func init() {
@@ -22,5 +24,133 @@ func BenchmarkParseWithoutCallbacks(b *testing.B) {
 			continue
 		}
 		log.Fatal(err)
+	}
+}
+
+func parseStringWithoutCallbacks(jsonString string) error {
+	reader := bytes.NewReader([]byte(jsonString))
+	evLJsonParser := NewParser()
+	return evLJsonParser.Parse(reader)
+}
+
+func TestInvalidJsonEmpty(t *testing.T) {
+	err := parseStringWithoutCallbacks("")
+	if err == nil {
+		t.Fail()
+	}
+}
+
+func TestStartObjects(t *testing.T) {
+	testCases := []string{
+		"0",
+		"1",
+		"null",
+		"true",
+		"false",
+	}
+	for _, str := range testCases {
+		t.Logf(LOG_STMT_FMT, str)
+		err := parseStringWithoutCallbacks(str)
+		if err == nil {
+			t.FailNow()
+		}
+	}
+}
+
+func TestStrangeValidJson(t *testing.T) {
+	/*
+
+		Unexpected format features:
+		1. exponent coefficients with any number of leading zeros
+		2. negative zero ( makes sense for floats / wanting easy parsing )
+		    but it is not clear why this is supported in exponent coefficients
+		3. empty dict keys
+
+	*/
+	testCases := []string{
+		"[-0]",
+		"[0.0e0]",
+		"[0.0e00]",
+		"[0.0e001]",
+		"[-0.0e0]",
+		"[-0.0e00]",
+		"[-0.0e001]",
+		"[0.0e-0]",
+		"[0.0e-00]",
+		"[0.0e-001]",
+		"[-0.0e-0]",
+		"[-0.0e-00]",
+		"[-0.0e-001]",
+		"{\"\":-0}",
+		"{\"\":0.0e0}",
+		"{\"\":0.0e00}",
+		"{\"\":0.0e001}",
+		"{\"\":-0.0e0}",
+		"{\"\":-0.0e00}",
+		"{\"\":-0.0e001}",
+		"{\"\":0.0e-0}",
+		"{\"\":0.0e-00}",
+		"{\"\":0.0e-001}",
+		"{\"\":-0.0e-0}",
+		"{\"\":-0.0e-00}",
+		"{\"\":-0.0e-001}",
+		"{\"\":null}",
+	}
+	for _, str := range testCases {
+		t.Logf(LOG_STMT_FMT, str)
+		err := parseStringWithoutCallbacks(str)
+		if err != nil {
+			t.FailNow()
+		}
+	}
+}
+
+func TestNormalValidJson(t *testing.T) {
+	testCases := []string{
+		"[0]",
+		"[1]",
+		"[true]",
+		"[false]",
+		"[null]",
+		"{\"a\":0}",
+		"{\"a\":1}",
+		"{\"a\":true}",
+		"{\"a\":false}",
+		"{\"a\":null}",
+	}
+	for _, str := range testCases {
+		t.Logf(LOG_STMT_FMT, str)
+		err := parseStringWithoutCallbacks(str)
+		if err != nil {
+			t.FailNow()
+		}
+	}
+}
+
+func TestBadJson(t *testing.T) {
+	testCases := []string{
+		"[00]",
+		"[-00]",
+		"[01]",
+		"[-01]",
+		"[0",
+		"{\"\":0",
+		"[0,",
+		"{\"\":0,",
+		"[0,]",
+		"{\"\":0,}",
+		"{\"\":0,\"\"}",
+		"{\"\":0,\"a\"}",
+		"{\"\":0,\"\":}",
+		"{\"\":0,\"a\":}",
+		"{\"\":0,\"\":,}",
+		"{\"\":0,\"a\":,}",
+	}
+	for _, str := range testCases {
+		t.Logf(LOG_STMT_FMT, str)
+		err := parseStringWithoutCallbacks(str)
+		if err == nil {
+			t.FailNow()
+		}
 	}
 }
